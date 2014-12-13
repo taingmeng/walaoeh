80eda0808da8816c8115e06214ad6fe9d050bb31
Thearith <sothearith.sreang@gmail.com>
2014-12-13 15:28:04 +0800

    get timeline working

--- app/src/main/java/com/example/walaoeh/Timeline.java
+++ app/src/main/java/com/example/walaoeh/Timeline.java
@@ -1,17 +1,74 @@
 package com.example.walaoeh;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.example.walaoeh.helper.Const;
+import com.example.walaoeh.helper.Pref;
 
 
 public class Timeline extends Activity {
+
+    private int stage;
+    private int level;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        Pref.init(this);
+
+        stage = Pref.getPlayerStage();
+        level = Pref.getPlayerLevel();
+
+        TableLayout timelineLayout = (TableLayout) findViewById(R.id.activity_timeline_layout);
+        for(int stageIndex=0; stageIndex< Const.QUESTIONS[stage].length; stageIndex++){
+
+            TableRow row= new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            inflater.inflate(R.layout.activity_timeline_stage, row);
+
+            TextView stageTextView = (TextView) row.findViewById(R.id.stageTextView);
+            Button stageButton = (Button) row.findViewById(R.id.stageButton);
+            View stageLine = row.findViewById(R.id.stageLine);
+
+            stageTextView.setText(Const.STAGE_NAME[stageIndex]);
+
+            if(stageIndex < stage) {
+                stageButton.setBackgroundResource(R.drawable.round_button_complete);
+                stageButton.setText("100%");
+            } else if(stageIndex == stage) {
+                stageButton.setBackgroundResource(R.drawable.round_button_progress);
+                double percentage = ((double)level/((double)Const.QUESTIONS[stage].length/2))*100;
+                stageButton.setText(String.valueOf(level)+"%");
+            } else {
+                stageButton.setBackgroundResource(R.drawable.round_button_not_arrived);
+                stageButton.setText("??");
+            }
+
+            if(stageIndex == Const.QUESTIONS[stage].length -1) {
+                stageLine.setVisibility(View.GONE);
+            } else if(stageIndex < stage) {
+                stageLine.setBackgroundColor(getResources().getColor(R.color.stage_completed));
+            } else {
+                stageLine.setBackgroundColor(getResources().getColor(R.color.stage_not_arrived));
+            }
+
+            timelineLayout.addView(row, stageIndex);
+        }
     }
 
 
