6446d903d12a373a171647a3aa5eca079d8d2cf4
yslidet <seanglid1@e.ntu.edu.sg>
2014-12-13 10:25:06 +0800

    color

--- app/src/main/java/com/example/walaoeh/Splash.java
+++ app/src/main/java/com/example/walaoeh/Splash.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 public class Splash extends Activity {
 
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
